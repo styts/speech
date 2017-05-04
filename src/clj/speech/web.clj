12c8c34c4384f640c156071059487cffffcb7cfd
Kirill Stytsenko <kirill@styts.com>
2017-05-04 19:47:29 +0200

    Remove example handler

--- src/clj/speech/web.clj
+++ src/clj/speech/web.clj
@@ -31,16 +31,6 @@
         (send! channel (str audio "\n") false)
         (recur)))))
 
-(defn stream-handler [request]
-  (with-channel request channel
-    (on-close channel (fn [status] (println "channel closed, " status)))
-    (loop [id 0]
-      (when (< id 2000)
-        (schedule-task (* id 2)
-                       (send! channel (str "message from server #" id "\n") false)) ; false => don't close after send
-        (recur (inc id))))
-    (schedule-task 10000 (close channel)))) ;; close in 10s.
-
 (defn send-data-to-ws [data]
   (doseq [channel (keys @channel-hub)]
     (send! channel data)))
@@ -64,7 +54,6 @@
 (defroutes app
   (GET "/ws" [] ws-handler)
 
-  (GET "/stream" [] stream-handler)
   (GET "/live" [] live-handler)
 
   (not-found "<h1>Page not found</h1>"))
