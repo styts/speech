0f6dc39d1293c849ad390244e6bc2e8e04fe81cf
Kirill Stytsenko <kirill@styts.com>
2017-05-07 15:22:36 +0200

    Display a fram chart for debugging
    Use this to activate from user.clj:
    
    (ws-send {:frame (<!! audio-channel)})

--- src/cljs/speech/frames.cljs
+++ src/cljs/speech/frames.cljs
@@ -1,4 +1,27 @@
-(ns speech.frames)
+(ns speech.frames
+  (:require [clojure.core :refer [prn]]))
+
+(enable-console-print!)
+
+(def chart-options {:height "300px"
+                    :fullWidth true
+                    :showArea true
+                    :showPoint false
+                    :lineSmooth false
+                    :axisX {:showGrid false}
+                    :axisY {:showGrid false}
+                    ;; :low 0
+                    })
+
+(defn mount-chart [element data]
+  (def chart (js/Chartist.Line.
+              element
+              (clj->js {:series [data]})
+              (clj->js chart-options))))
 
 (defn draw-live-frame [frame]
-  )
+  (prn frame)
+  (let [cnt (.getElementById js/document "container")
+        chart (.getElementById js/document "frame-chart")
+        ]
+    (mount-chart chart frame)))
